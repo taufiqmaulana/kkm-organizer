1a13aedee83a6914e18ae85cba4e567fd0b1dc9b
taufiqmaulana <fixy.maulana@gmail.com>
2025-11-08 01:48:22 +0700

    add homepage menu

--- src/routes/app/index.tsx
+++ src/routes/app/index.tsx
@@ -1,9 +1,19 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
+import { Tent } from 'lucide-react'
 
 export const Route = createFileRoute('/app/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/app/"!</div>
+  return (
+    <div className="flex">
+      <div className="card shadow hover:shadow-xl">
+        <Link to="/app/events" className="card-body">
+          <Tent size={32} />
+          <span>Manage Event</span>
+        </Link>
+      </div>
+    </div>
+  )
 }
