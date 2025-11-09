7239d5f82a703608ef3e8bf637ed4286554ea39c
taufiqmaulana <fixy.maulana@gmail.com>
2025-11-09 17:53:35 +0700

    improve home menu UI visibility

--- src/routes/app/index.tsx
+++ src/routes/app/index.tsx
@@ -7,11 +7,11 @@ export const Route = createFileRoute('/app/')({
 
 function RouteComponent() {
   return (
-    <div className="flex">
-      <div className="card shadow hover:shadow-xl">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+      <div className="card shadow hover:shadow-xl bg-base-100">
         <Link to="/app/events" className="card-body">
           <Tent size={32} />
-          <span>Manage Event</span>
+          <span className="text-lg">Manage Events</span>
         </Link>
       </div>
     </div>
